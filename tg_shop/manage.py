a5a39fbc35d57ac657ed2ec8924cb647d450d80f
belovni@nornik.ru <belovni@nornik.ru>
2025-06-16 16:35:00 +0300

    Go

--- tg_shop/manage.py
+++ tg_shop/manage.py
@@ -2,7 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
+import os
 
+dotenv.load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
 
 def main():
     """Run administrative tasks."""
