80d1cec07965c55e060565cfac39a084631cbb7d
belovni@nornik.ru <belovni@nornik.ru>
2025-06-29 17:45:32 +0300

    InitFastAPI

--- fastapi_app/app/config.py
+++ fastapi_app/app/config.py
@@ -1,4 +1,4 @@
-from pydantic import BaseSettings
+from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     """Конфигурация приложения, загруженная из окружения."""
@@ -11,5 +11,6 @@ class Settings(BaseSettings):
 
     class Config:
         env_file = ".env"
+        extra = "allow"  # или "ignore"
 
 settings = Settings()
