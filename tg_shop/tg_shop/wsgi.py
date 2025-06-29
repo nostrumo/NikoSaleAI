5de2826000008079b674d9dd7c2d7080f9f5f705
nostrumo <45200960+nostrumo@users.noreply.github.com>
2025-06-29 16:39:41 +0300

    fix(env): load dotenv for uvicorn

--- tg_shop/tg_shop/wsgi.py
+++ tg_shop/tg_shop/wsgi.py
@@ -1,16 +1,12 @@
-"""
-WSGI config for tg_shop project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
+"""WSGI-приложение для проекта tg_shop."""
 
 import os
+from pathlib import Path
 
+from dotenv import load_dotenv
 from django.core.wsgi import get_wsgi_application
 
+load_dotenv(Path(__file__).resolve().parent.parent / ".env")
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tg_shop.settings')
 
 application = get_wsgi_application()
